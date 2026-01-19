724cf276c233a73aec177d6d927d86a5cfaed699
scmbr <romvasily@mail.ru>
2026-01-20 02:40:57 +0900

    feat: add subscription domain constructor with invariants checking

--- internal/domain/subscription.go
+++ internal/domain/subscription.go
@@ -1,6 +1,9 @@
 package domain
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 type Subscription struct {
 	Id          string
@@ -9,4 +12,21 @@ type Subscription struct {
 	UserID      string
 	StartDate   time.Time
 	EndDate     *time.Time
+}
+
+func NewSubscription(id, serviceName string, price int, userID string, startDate time.Time, endDate *time.Time) (*Subscription, error) {
+	if endDate != nil && endDate.Before(startDate) {
+		return nil, fmt.Errorf("domain.NewSubscription invalid startDate and endDate")
+	}
+	if price < 0 {
+		return nil, fmt.Errorf("domain.NewSubscription invalid price")
+	}
+	return &Subscription{
+		Id:          id,
+		ServiceName: serviceName,
+		Price:       price,
+		UserID:      userID,
+		StartDate:   startDate,
+		EndDate:     endDate,
+	}, nil
 }
