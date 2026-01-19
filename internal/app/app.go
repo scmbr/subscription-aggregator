9a766d66b1c27573f217c802962522b19c02abd2
scmbr <romvasily@mail.ru>
2026-01-20 01:04:19 +0900

    feat: add db connection in app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"github.com/scmbr/subscription-aggregator/internal/config"
+	"github.com/scmbr/subscription-aggregator/pkg/database/postgres"
 	"github.com/scmbr/subscription-aggregator/pkg/logger"
 )
 
@@ -15,5 +16,20 @@ func Run(configsDir string) {
 	logger.Info("configs initialized successfully", map[string]interface{}{
 		"http_port":     cfg.HTTP.Port,
 		"postgres_port": cfg.Postgres.Port,
+	})
+
+	db, err := postgres.NewPostgresDB(postgres.Config{
+		Host:     cfg.Postgres.Host,
+		Port:     cfg.Postgres.Port,
+		Username: cfg.Postgres.Username,
+		Password: cfg.Postgres.Password,
+		DBName:   cfg.Postgres.Name,
+		SSLMode:  cfg.Postgres.SSLMode,
+	})
+	if err != nil {
+		logger.Error("failed to connect to database", err, nil)
+	}
+	logger.Info("connected to database successfully", map[string]interface{}{
+		"database is connected": db.DB.Ping() == nil,
 	})
 }
