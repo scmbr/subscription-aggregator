72b7e15d0612f72b1cf8db5434c6177171f3ed51
scmbr <romvasily@mail.ru>
2026-01-20 04:33:41 +0900

    feat: add repository,service,handler and server in app.go

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,7 +1,19 @@
 package app
 
 import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
 	"github.com/scmbr/subscription-aggregator/internal/config"
+	"github.com/scmbr/subscription-aggregator/internal/delivery/http/handler"
+	"github.com/scmbr/subscription-aggregator/internal/repository"
+	"github.com/scmbr/subscription-aggregator/internal/server"
+	"github.com/scmbr/subscription-aggregator/internal/service"
 	"github.com/scmbr/subscription-aggregator/pkg/database/postgres"
 	"github.com/scmbr/subscription-aggregator/pkg/logger"
 )
@@ -32,4 +44,23 @@ func Run(configsDir string) {
 	logger.Info("connected to database successfully", map[string]interface{}{
 		"database is connected": db.DB.Ping() == nil,
 	})
+	repository := repository.NewRepository(db)
+	service := service.NewService(repository)
+	handler := handler.NewHandler(service)
+	server := server.NewServer(cfg, handler.Init())
+	go func() {
+		if err := server.Run(); !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("error occurred while running server", err, nil)
+		}
+	}()
+	logger.Info("server started", nil)
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+	const timeout = 5 * time.Second
+	ctx, shutdown := context.WithTimeout(context.Background(), timeout)
+	defer shutdown()
+	if err := server.Stop(ctx); err != nil {
+		logger.Error("failed to stop server", err, nil)
+	}
 }
