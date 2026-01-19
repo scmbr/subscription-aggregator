3f4f49626a8c092dea75c2b895b88845f33d901d
scmbr <romvasily@mail.ru>
2026-01-20 04:24:03 +0900

    feat: add handler dto

---
+++ internal/delivery/http/handler/dto/subscription.go
@@ -0,0 +1,31 @@
+package dto
+
+type CreateSubscriptionRequest struct {
+	ServiceName string     `json:"service_name" binding:"required"`
+	Price       int        `json:"price" binding:"required,gte=0"`
+	UserID      string     `json:"user_id" binding:"required,uuid4"`
+	StartDate   MonthYear  `json:"start_date" binding:"required"`
+	EndDate     *MonthYear `json:"end_date" binding:"omitempty"`
+}
+type CreateSubscriptionResponse struct {
+	Id string `json:"subscription_id"`
+}
+type GetAllSubscriptionsResponse struct {
+	Total         int                       `json:"total"`
+	Subscriptions []GetSubscriptionResponse `json:"subscriptions"`
+}
+type GetSubscriptionResponse struct {
+	Id          string     `json:"subscription_id"`
+	ServiceName string     `json:"service_name"`
+	Price       int        `json:"price"`
+	UserID      string     `json:"user_id"`
+	StartDate   MonthYear  `json:"start_date"`
+	EndDate     *MonthYear `json:"end_date"`
+}
+type UpdateSubscriptionRequest struct {
+	ServiceName *string    `json:"service_name"  binding:"omitempty"`
+	Price       *int       `json:"price"  binding:"omitempty,gte=0"`
+	UserID      *string    `json:"user_id"  binding:"omitempty"`
+	StartDate   *MonthYear `json:"start_date"  binding:"omitempty"`
+	EndDate     *MonthYear `json:"end_date"  binding:"omitempty"`
+}
