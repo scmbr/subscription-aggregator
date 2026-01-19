1904ae6f1e8d1b3d69d92b35e2fd014a68d02724
scmbr <romvasily@mail.ru>
2026-01-20 01:19:58 +0900

    fix: fix migration

--- migrations/000001_create_subscriptions_table.up.sql
+++ migrations/000001_create_subscriptions_table.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE subscriptions(
     price integer NOT NULL,
     user_id UUID NOT NULL,
     start_date  TIMESTAMPTZ NOT NULL
+    end_date TIMESTAMPTZ DEFAULT NULL
 );
 CREATE INDEX idx_subscriptions_user_id ON subscriptions(user_id);
