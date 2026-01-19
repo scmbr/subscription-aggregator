9903c16b2df09b115615cdbf143d3e8645a47181
scmbr <romvasily@mail.ru>
2026-01-19 23:58:53 +0900

    feat: add migrations

---
+++ migrations/000001_create_subscriptions_table.up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE subscriptions(
+    id UUID PRIMARY KEY,
+    service_name VARCHAR(30) NOT NULL,
+    price integer NOT NULL,
+    user_id UUID NOT NULL,
+    start_date  TIMESTAMPTZ NOT NULL
+);
+CREATE INDEX idx_subscriptions_user_id ON subscriptions(user_id);
